1872af87410ffdab9440bcc5cfcc91df5bcd1c5c
kmtym1998 <kmtym.devloper@gmail.com>
2022-11-13 14:29:52 +0900

    history のstyle を整えた

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,22 +1,44 @@
 import { Timeline } from 'antd';
+import '../styles/history.css';
 import 'antd/dist/antd.css';
 
 export const History: React.FC = () => {
   return (
-    <>
+    <div>
       <Timeline mode="left" pending pendingDot={<></>}>
-        <Timeline.Item label={'2020-07'}>
-          株式会社 BuySell Technologies 入社 (インターン)
+        <Timeline.Item>
+          <p className="timelineCaption">2017-03</p>
+          <p className="timelineTitle">千葉市立千葉高等学校卒業</p>
         </Timeline.Item>
 
-        <Timeline.Item label={'2021-03'}>
-          名古屋大学情報学部人間社会情報学科 卒業
+        <Timeline.Item>
+          <p className="timelineCaption">2017-04</p>
+          <p className="timelineTitle">
+            名古屋大学情報学部人間社会情報学科 入学
+          </p>
         </Timeline.Item>
 
-        <Timeline.Item label={'2021-04'}>
-          株式会社 BuySell Technologies 入社 (正社員)
+        <Timeline.Item>
+          <p className="timelineCaption">2020-07</p>
+          <p className="timelineTitle">
+            株式会社 BuySell Technologies 入社 (インターン)
+          </p>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <p className="timelineCaption">2021-03</p>
+          <p className="timelineTitle">
+            名古屋大学情報学部人間社会情報学科 卒業
+          </p>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <p className="timelineCaption">2021-04</p>
+          <p className="timelineTitle">
+            株式会社 BuySell Technologies 入社 (正社員)
+          </p>
         </Timeline.Item>
       </Timeline>
-    </>
+    </div>
   );
 };
