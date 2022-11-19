ac2388c49578cdbe4eff072cbbaab3ed1d2bf505
kmtym1998 <kmtym.devloper@gmail.com>
2022-11-19 22:27:03 +0900

    記事

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,35 @@
+import { Card } from 'antd';
+import type { Metadata } from '../articlesConfig/type';
+import 'antd/dist/antd.css';
+
+type Props = {
+  date: string;
+  metadata: Metadata;
+  bodyStyle?: React.CSSProperties;
+  style?: React.CSSProperties;
+};
+
+export const ArticleCard = ({ date, bodyStyle, metadata, style }: Props) => {
+  return (
+    <a href={metadata.url} target="_blank">
+      <Card
+        style={style}
+        cover={
+          <img src={metadata.image} style={{ border: '1px solid #f0f0f0' }} />
+        }
+        hoverable
+        bodyStyle={bodyStyle}
+      >
+        <span>{metadata.title}</span>
+        <br />
+        <span
+          style={{
+            color: '#bbb',
+          }}
+        >
+          {date}
+        </span>
+      </Card>
+    </a>
+  );
+};
