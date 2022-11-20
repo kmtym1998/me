858bd979a4ee08207a1c8076dfc71bc328ead718
kmtym1998 <kmtym.devloper@gmail.com>
2022-11-20 18:05:14 +0900

    add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   site: 'https://kmtym1998.github.io',
   base: '/me',
 });
